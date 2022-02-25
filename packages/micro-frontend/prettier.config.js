edae311eff8a4592d37df610a7be53160c5ec759
Won Song <wosong@microsoft.com>
2022-02-25 13:48:33 -0800

    Workspace setup

--- packages/micro-frontend/prettier.config.js
+++ packages/micro-frontend/prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     semi: true,
     singleQuote: true,
-    tabWidth: 4,
+    tabWidth: 2,
     printWidth: 120,
 };
--- packages/micro-frontend/src/OnBuildOnce.ts
+++
@@ -1,1 +0,0 @@
-export * from './Adaptors/BuildOnce/OnBuildOnce';
